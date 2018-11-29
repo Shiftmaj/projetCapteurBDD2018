c257b1d6854accda397f893f861959b7838f89c8
Lucas PARISOT <lucas-parisot@orange.fr>
2018-11-28 20:25:41 -0500

    Ajout de header post

--- php/parametre/modifier.php
+++ php/parametre/modifier.php
@@ -1,6 +1,12 @@
 <?php
     include "../accesseur/ParametreDAO.php";
 
+    // headers requis
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: POST");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
     
     if (isset($_POST['superieur_a']) && isset($_POST['inferieur_a'])) {
 
