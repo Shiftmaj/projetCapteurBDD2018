0da5f57bf339317be01d45d63c6ce21bd4e1abd8
Mitsuriou <dylanpro.jacquemin@gmail.com>
2018-12-05 17:26:07 -0500

    Changements des URLs et des identifiants de Base de données

--- php/accesseur/connexion.php
+++ php/accesseur/connexion.php
@@ -1,10 +1,10 @@
 <?php
-    $hostname = "54.39.144.87";
+    $hostname = "54.37.152.134";
     $databaseName = "capture";
     $port = "5432";
     
     $username = "postgres";
-    $password = "9182";
+    $password = "PG@546582";
 
     $dsn = 'pgsql:dbname='.$databaseName.';host='.$hostname.';port='.$port;
     $basededonnees = new PDO($dsn, $username, $password);
