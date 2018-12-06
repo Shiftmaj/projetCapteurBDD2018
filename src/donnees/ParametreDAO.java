f9514b9814d265a2e2d280ce4b5674d6bb3013d0
Mitsuriou <dylanpro.jacquemin@gmail.com>
2018-12-05 19:11:01 -0500

    Méthode PUT pour les éléments + factorisation du code

--- src/donnees/ParametreDAO.java
+++ src/donnees/ParametreDAO.java
@@ -3,19 +3,15 @@ package donnees;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
-import java.util.StringJoiner;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import controleur.ControleurPrincipal;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -26,82 +22,62 @@ import modele.Parametre;
 
 public class ParametreDAO {
 
-    String xml = null;
-    ControleurPrincipal controleurPrincipal = ControleurPrincipal.getInstance();
+    private String xml;
 
     public ParametreDAO() {
-
+        this.xml = null;
     }
 
-    public void modifierParametre(int heures, int element, float superieurA, float inferieurA, boolean boolHeure){
+    public void modifierParametre(int heures, int elements, float superieurA, float inferieurA, boolean boolHeure){
 
         if(inferieurA>superieurA){
+            JSONObject objet = new JSONObject();
+
             if(boolHeure){
                 if(heures > 0){
                     try {
-                        // Création du Json
-                        JSONObject objet = new JSONObject();
                         objet.put("inferieur_a", inferieurA);
                         objet.put("superieur_a", superieurA);
                         objet.put("heures", heures);
-
-                        System.out.println(objet.toString());
-                        System.out.println(superieurA);
-
-                        // Contact à l'api
-                        URL url = new URL("https://papi.capture.tenam.re/parametre");
-                        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                        con.setDoOutput(true);
-                        con.setRequestMethod("PUT");
-                        con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-                        con.setRequestProperty("Accept", "application/json");
-
-                        OutputStream os = con.getOutputStream();
-                        os.write(objet.toString().getBytes(StandardCharsets.UTF_8));
-
-                        // NECESSAIRE POUR L'API :
-                        con.getResponseMessage();
-                        
-                        os.close();
-
-                    }catch (Exception e){
+                    }
+                    catch (JSONException e){
                         e.printStackTrace();
-                        System.out.println("NON");
                     }
                 }
             }
             else {
-                if(element > 2){
+                if(elements > 2){
                     try {
-                        URL url = new URL("https://papi.capture.tenam.re/parametre");
-                        URLConnection con = url.openConnection();
-                        HttpURLConnection http = (HttpURLConnection)con;
-                        http.setRequestMethod("PUT");
-                        http.setDoOutput(true);
-
-                        Map<String,String> arguments = new HashMap<>();
-                        arguments.put("quantite_entree", "" + element);
-                        arguments.put("superieur_a", "" + superieurA);
-                        arguments.put("inferieur_a", "" + inferieurA);
-
-                        StringJoiner sj = new StringJoiner("&");
-                        for(Map.Entry<String,String> entree: arguments.entrySet())
-                            sj.add(URLEncoder.encode(entree.getKey(),"UTF-8") + "="
-                                    + URLEncoder.encode(entree.getValue(), "UTF-8"));
-                        byte[] sortie = sj.toString().getBytes(StandardCharsets.UTF_8);
-                        int longueur = sortie.length;
-
-                        http.setFixedLengthStreamingMode(longueur);
-                        http.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-                        http.connect();
-                        try(OutputStream flux = http.getOutputStream()){
-                            flux.write(sortie);
-                        }
-                    }catch (Exception e){
+                        objet.put("inferieur_a", inferieurA);
+                        objet.put("superieur_a", superieurA);
+                        objet.put("quantite_entree", elements);
+                    }
+                    catch (JSONException e){
                         e.printStackTrace();
-                        System.out.println("NON");
                     }
                 }
+            }
+
+            try {
+                // Contact à l'api
+                URL url = new URL("https://papi.capture.tenam.re/parametre");
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setDoOutput(true);
+                con.setRequestMethod("PUT");
+                con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                con.setRequestProperty("Accept", "application/json");
+
+                OutputStream os = con.getOutputStream();
+                os.write(objet.toString().getBytes(StandardCharsets.UTF_8));
+
+                // NECESSAIRE POUR L'API :
+                con.getResponseMessage();
+
+                os.close();
+            }
+            catch (Exception e){
+                e.printStackTrace();
+                System.out.println("NON");
             }
         }
     }
